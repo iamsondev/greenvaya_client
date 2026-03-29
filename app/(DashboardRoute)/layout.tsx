61b28c5c8757e434acc63fd427ac77b1135ee155
iamsondev <sondipkumarsk@gmail.com>
2026-03-29 22:41:56 +0600

    create layout for dashboardRoute and create route for admin and member

---
+++ app/(DashboardRoute)/layout.tsx
@@ -0,0 +1,15 @@
+
+
+export default function DashboardLayout({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+    return (
+        <div>
+
+            {children}
+
+        </div>
+    )
+}
